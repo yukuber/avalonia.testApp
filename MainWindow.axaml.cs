5330ba0ba8544d683d39495571fcf73046014bd7
DimaTrikh <d.trikhunkov@outlook.com>
2024-04-17 12:25:29 +0300

    program 2

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Threading.Tasks;
+
+namespace MyFirstApp{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      public async void ButtonOne_OnClick(object sender, RoutedEventArgs args)
+    {
+        var ownerWindow = this;
+        var window = new WindowOne();
+        var dialogResult = await window.ShowDialog<bool>(ownerWindow);
+        TextBlockDialogResult.Text = dialogResult.ToString();
+    }
+    }
+}
---
+++ WindowOne.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace MyFirstApp{
+
+public partial class WindowOne : Window
+{
+    public WindowOne()
+    {
+        InitializeComponent();
+    }
+
+    public void ButtonOk_OnClick(object sender, RoutedEventArgs args)
+    {
+        Close(true);
+    }
+    public void ButtonCancel_OnClick(object sender, RoutedEventArgs args)
+    {
+        Close(false);
+    }
+}
+}
